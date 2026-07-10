b05eeb6aedacc8d0079809fdf59a440e5ea3a2d8
Tetiana <t.lebed17@gmail.com>
2026-07-10 23:18:53 +0300

    add global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { BooksComponent } from './books/books.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   imports: [BooksComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline', floatLabel: 'always' }
+    }
+  ]
 })
 export class AppComponent {
   title = 'BooksApp';
